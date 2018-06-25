5c8b08a9cec37bd621fe2016f8c5bef0c52bd8d4
Junichi Kimura <junichi@netspace.org>
2018-06-24 23:49:44 -0700

    Chapter 2 compiles and runs.

---
+++ vec3.h
@@ -0,0 +1,52 @@
+#include <math.h>
+#include <stdlib.h>
+#include <iostream>
+
+class vec3 {
+public:
+    vec3() {}
+    vec3(float e0, float e1, float e2) {e[0] = e0; e[1] = e1; e[2] = e2;}
+    inline float x() const {return e[0];}
+    inline float y() const {return e[1];}
+    inline float z() const {return e[2];}
+    inline float r() const {return x();}
+    inline float g() const {return y();}
+    inline float b() const {return z();}
+
+    inline const vec3& operator+() const {return *this;}
+    inline vec3 operator-() const {return vec3(-e[0], -e[1], -e[2]);}
+    inline float operator[](int i) const {return e[i];}
+
+    inline vec3& operator+=(const vec3 &v2);
+    inline vec3& operator-=(const vec3 &v2);
+    inline vec3& operator*=(const vec3 &v2);
+    inline vec3& operator/=(const vec3 &v2);
+    inline vec3& operator*=(const float t);
+    inline vec3& operator/=(const float t);
+
+    inline float length() const {
+        return sqrt(this->squared_length());
+    }    
+    inline float squared_length() const {
+        return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+    }
+
+    inline void make_unit_vector();
+    float e[3];
+};
+
+inline std::istream& operator>>(std::istream &is, vec3 &t) {
+    is >> t.e[0] >> t.e[1] >> t.e[2];
+    return is;
+}
+
+inline std::ostream& operator<<(std::ostream &os, vec3 &t) {
+    os << t.e[0] << " " << t.e[1] << " " << t.e[2];
+    return os;
+}
+
+inline void vec3::make_unit_vector() {
+    float k = 1.0 / this->length();
+    e[0] *= k; e[1] *= k; e[2] *= k; 
+}
+

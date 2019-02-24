01ba22d9ba263bd39faced685771916276d692ac
Junichi Kimura <junichi@netspace.org>
2019-02-23 22:54:35 -0800

    Adding build batch file for VS2017

---
+++ makeandrun.bat
@@ -0,0 +1,12 @@
+@SETLOCAL
+@CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+@CD /D %~dp0
+
+DEL ray.exe
+cl main.cpp /W4 /EHsc /link /out:ray.exe
+IF NOT EXIST ray.exe (
+  PAUSE
+  EXIT /B 1
+)
+ray.exe > output.ppm

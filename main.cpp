e70d3529e0988c39e0265143ba834a799b643e9f
Junichi Kimura <junichi@netspace.org>
2019-03-03 18:40:47 -0800

    chapter 5, part 1

--- main.cpp
+++ main.cpp
@@ -29,21 +29,37 @@ vec3 color_ch03(const ray& r) {
   return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3 (0.5, 0.7, 1.0); //lerp between white and blue
 }
 
-bool hit_sphere(const vec3& center, float radius, const ray& r) {
+float hit_sphere(const vec3& center, float radius, const ray& r) {
   vec3 oc = r.origin() - center;
   float a = dot(r.direction(), r.direction());
   float b = 2.0 * dot(oc, r.direction());
   float c = dot(oc, oc) - radius * radius;
   float discriminant = b * b - 4 * a * c;
-  return (discriminant > 0);
+  if (discriminant < 0) {
+    return -1.0;
+  }
+  return (-b - sqrt(discriminant)) / (2.0*a);
 }
 
-vec3 color(const ray& r) {
-  if (hit_sphere(vec3(0,0,-1), 0.5, r))
+vec3 color_ch04(const ray& r) {
+  if (hit_sphere(vec3(0,0,-1), 0.5, r) > 0.0)
     return vec3(1, 0, 0);
   vec3 unit_direction = unit_vector(r.direction());
   float t = 0.5 * (unit_direction.y() + 1.0); // -1.0 ~ 1.0 -> 0.0 ~ 1.0
   return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3 (0.5, 0.7, 1.0); //lerp between white and blue
+}
+
+vec3 color(const ray& r) {
+  float t = hit_sphere(vec3(0,0,-1), 0.5, r);
+  if (t > 0.0) {
+    vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0,0,-1)); //normal vector from center to hit point
+    return 0.5 * vec3(N.x()+1.0, N.y()+1.0, N.z()+1.0); //clamp to 0,1
+  }
+  else {
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = 0.5 * (unit_direction.y() + 1.0); // -1.0 ~ 1.0 -> 0.0 ~ 1.0
+    return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3 (0.5, 0.7, 1.0); //lerp between white and blue
+  }
 }
 
 int main() {

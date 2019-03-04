87c78b6b647187e1ad7e6bf8a5fcc4df187a4a85
Junichi Kimura <junichi@netspace.org>
2019-03-03 21:26:42 -0800

    chapter 5

---
+++ sphere.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "hitable.h"
+
+class sphere: public hitable {
+    public:
+        sphere() {}
+        sphere(vec3 center, float r): m_center(center), m_radius(r) {}
+        virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const;
+        vec3 m_center;
+        float m_radius;
+};
+
+bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
+  vec3 oc = r.origin() - m_center;
+  float a = dot(r.direction(), r.direction());
+  float b = dot(oc, r.direction());
+  float c = dot(oc, oc) - m_radius * m_radius;
+  float discriminant = b * b - a * c;
+  if (discriminant > 0) {
+      float temp = (-b - sqrt(discriminant)) / a;
+      if (temp < t_max && temp > t_min) {
+          rec.t = temp;
+          rec.p = r.point_at_parameter(rec.t);
+          rec.normal = (rec.p - m_center) / m_radius;
+          return true;
+      }
+      temp = (-b + sqrt(discriminant)) / a;
+      if (temp < t_max && temp > t_min) {
+          rec.t = temp;
+          rec.p = r.point_at_parameter(rec.t);
+          rec.normal = (rec.p - m_center) / m_radius;
+          return true;
+      }
+  }
+  return false;
+}

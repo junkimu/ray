e0df05cd741add27df2f17b405c4fb2002ee845b
Junichi Kimura <junichi@netspace.org>
2019-03-04 23:05:18 -0800

    Chapter 7

--- main.cpp
+++ main.cpp
@@ -27,10 +27,20 @@ void output_ppm(int nx, int ny, void* data) {
   }
 }
 
+vec3 random_in_unit_sphere() {
+  vec3 p;
+  do {
+    p = 2.0 * vec3(drand48(),drand48(),drand48()) - vec3(1,1,1);
+  } while (p.squared_length() >= 1.0);
+  return p;
+}
+
 vec3 color(const ray& r, hitable* world) {
+  float EPSILON = 0.001; //to ignore hits very near zero
   hit_record rec;
-  if (world->hit(r, 0.0, FLT_MAX, rec)) {
-    return 0.5 * vec3(rec.normal.x()+1.0, rec.normal.y()+1.0, rec.normal.z()+1.0); //clamp to 0,1
+  if (world->hit(r, EPSILON, FLT_MAX, rec)) {
+    vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+    return 0.5 * color(ray(rec.p, target - rec.p), world);
   }
   else {
     vec3 unit_direction = unit_vector(r.direction());
@@ -62,6 +72,7 @@ int main() {
         col += color(r, world);
       }
       col /= float(numsamples);
+      col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2])); //gamma correct
       int ir = int(255.99 * col[0]);
       int ig = int(255.99 * col[1]);
       int ib = int(255.99 * col[2]);

cf81055e627bcc343d7e522d7b2beb5476420e71
Junichi Kimura <junichi@netspace.org>
2019-03-07 03:07:13 -0800

    Support VS2017 Pro.  Add optimization flag.  Don't always recompile stb_image_write.cpp because it's slow.

--- makeandrun.bat
+++ makeandrun.bat
@@ -1,12 +1,14 @@
 @SETLOCAL
-@CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+@IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+@IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat" CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat"
 
 @CD /D %~dp0
 
 DEL ray.exe output.png output.ppm
-cl main.cpp stb_image_write.cpp /W4 /EHsc /link /out:ray.exe
+IF NOT EXIST stb_image_write.obj (cl /nologo /c stb_image_write.cpp)
+cl main.cpp /nologo /O2 /W4 /EHsc /link stb_image_write.obj /out:ray.exe
 IF NOT EXIST ray.exe (
   PAUSE
   EXIT /B 1
 )
-ray.exe > output.ppm
+ray.exe

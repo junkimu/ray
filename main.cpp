a687c68ba34c3516a8249697ca15e2609479cb69
Junichi Kimura <junichi@netspace.org>
2020-06-23 01:58:22 -0700

    Chapter 12 - Defocus blur

--- main.cpp
+++ main.cpp
@@ -126,8 +126,12 @@ int main( int argc, char *argv[]) {
   world.add(make_shared<triangle>(
     vec3(0,-0.5,0), vec3(0,-0.5,-2), vec3(-1,-0.5,0), vec3(0,1,0),make_shared<metal>(color(0.3f, 0.3f, 0.7f),0.0f)));
 */
-
-  camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), 20, aspect_ratio);
+  point3 lookfrom(3,3,2);
+  point3 lookat(0,0,-1);
+  vec3 vup(0,1,0);
+  auto dist_to_focus = (lookfrom-lookat).length();
+  auto aperture = 2.0;
+  camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
   std::cerr << "spawning " << num_threads << " threads \n";
   std::thread *threads = new std::thread[num_threads];
--- vec3.h
+++ vec3.h
@@ -152,6 +152,21 @@ inline vec3 unit_vector(const vec3& v) {
     return v / v.length();
 }
 
+vec3 random_in_unit_disk() {
+    while (true) {
+        auto p = vec3(random_double(-1,1), random_double(-1,1), 0);
+        if (p.length_squared() >= 1) continue;
+        return p;
+    }
+}
+
+vec3 random_unit_vector() {
+    auto a = random_double(0, 2*pi);
+    auto z = random_double(-1, 1);
+    auto r = sqrt(1 - z*z);
+    return vec3(r*cos(a), r*sin(a), z);
+}
+
 vec3 random_in_unit_sphere() {
   vec3 p;
   do {

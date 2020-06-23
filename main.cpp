073b6461936e3d24a7a48692e7a058d764bace0b
Junichi Kimura <junichi@netspace.org>
2020-06-23 02:16:01 -0700

    Chapter 13 - Where next?

--- main.cpp
+++ main.cpp
@@ -88,6 +88,52 @@ void raytrace_thread(int thread_id, int nx, int ny, int comp, int numsamples, ca
   }
 }
 
+hittable_list random_scene() {
+    hittable_list world;
+
+    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = random_double();
+            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+
+            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<material> sphere_material;
+
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    sphere_material = make_shared<lambertian>(albedo);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else {
+                    // glass
+                    sphere_material = make_shared<dielectric>(1.5);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
+
+    auto material1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+    return world;
+}
+
 
 int main( int argc, char *argv[]) {
   (void)argc;
@@ -108,29 +154,13 @@ int main( int argc, char *argv[]) {
   unsigned char* p = data;
   const int max_depth = 50;
 
-  hittable_list world;
-  world.add(make_shared<sphere>(
-    point3(0.0f, 0.0f ,-1.0f), 0.5f, make_shared<lambertian>(color(0.1f, 0.2f, 0.5f))));
-  world.add(make_shared<sphere>(
-    point3(0.0f,-100.5f,-1.0f), 100.0f, make_shared<lambertian>(color(0.8f, 0.8f, 0.0f))));
-  world.add(make_shared<sphere>(
-    point3(1.0f,0.0f,-1.0f), 0.5f, make_shared<metal>(color(0.8f, 0.6f, 0.2f), 0.3f)));
-  world.add(make_shared<sphere>(
-    point3(-1.0f,0.0f,-1.0f), 0.5f, make_shared<dielectric>(1.5f)));
-  world.add(make_shared<sphere>(
-    point3(-1.0f,0.0f,-1.0f), -0.45f, make_shared<dielectric>(1.5f)));
-
-/*
-  world.add(make_shared<triangle>(
-    vec3(0,-0.5,0), vec3(0,-0.5,-2), vec3(1,-0.5,0), vec3(0,1,0),make_shared<lambertian>(color(0.3f, 0.3f, 0.7f))));
-  world.add(make_shared<triangle>(
-    vec3(0,-0.5,0), vec3(0,-0.5,-2), vec3(-1,-0.5,0), vec3(0,1,0),make_shared<metal>(color(0.3f, 0.3f, 0.7f),0.0f)));
-*/
-  point3 lookfrom(3,3,2);
-  point3 lookat(0,0,-1);
+  hittable_list world = random_scene();
+  point3 lookfrom(13,2,3);
+  point3 lookat(0,0,0);
   vec3 vup(0,1,0);
-  auto dist_to_focus = (lookfrom-lookat).length();
-  auto aperture = 2.0;
+  auto dist_to_focus = 10.0;
+  auto aperture = 0.1;
+
   camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
   std::cerr << "spawning " << num_threads << " threads \n";
